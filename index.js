d11689a449038ae93622d2fc40095cfd742edf1d
brettmadrid <brettmadrid9@gmail.com>
2019-02-28 00:07:06 -0800

    added put endpoint - MVP complete

--- index.js
+++ index.js
@@ -69,6 +69,28 @@ server.post('/api/users', (req, res) => {
   });
 })
 
+server.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body
+  const properUser = (updatedUser.name && updatedUser.bio) ? true : false;
+
+  db.update(id, updatedUser)
+  .then(updated => {
+    if (updated && properUser) {
+      res.json(updated)
+    } else {
+      if (!properUser) {
+        res.status(400).json({err: "Please provide name and bio for the user."})
+      } else {
+        res.status(404).json({err: "The user with the specified ID does not exist."})
+      }
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ error: "There was an error while saving the user to the database" });
+  });
+})
+
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
